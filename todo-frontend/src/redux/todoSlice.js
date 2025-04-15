84be1bb83bb9de7a3f729738d9efbd2976b9d770
Aftab Bashir <aftab48554@gmail.com>
2025-04-15 12:47:30 +0500

    Update todoSlice.js

--- todo-frontend/src/redux/todoSlice.js
+++ todo-frontend/src/redux/todoSlice.js
@@ -5,6 +5,7 @@ const backendBaseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
 
 const API = axios.create({
   baseURL: `${backendBaseUrl}/api`,
+  withCredentials: true,
 });
 
 // Add token to headers
